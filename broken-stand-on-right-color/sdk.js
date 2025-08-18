7c20fca6adf24b8840d8fac411040eb6a4bf72a1
braybray <braydenhuckle13@gmail.com>
2025-08-18 19:51:24 -0400

    67777

---
+++ broken-stand-on-right-color/sdk.js
@@ -0,0 +1,2 @@
+// Minimal SDK stub - actual implementation is in mock-yagames.js
+console.log("SDK.js loaded - using mock implementation");
--- stand-on-right-color/sdk.js
+++
@@ -1,2 +0,0 @@
-// Minimal SDK stub - actual implementation is in mock-yagames.js
-console.log("SDK.js loaded - using mock implementation");
